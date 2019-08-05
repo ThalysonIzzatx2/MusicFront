dbed22558c8a899bd6d21373b384a1bacd1df3b8
thalysonizzatx2 <thalyson.gabriel.a.sousa@gmail.com>
2019-08-05 00:12:24 -0300

    Update

--- src/pages/notes/index.js
+++ src/pages/notes/index.js
@@ -3,6 +3,8 @@ import api from '../../services/api';
 import { MdChevronRight } from 'react-icons/md'
 import ReactPlayer from 'react-player'
 import { withRouter } from 'react-router-dom';
+
+import loading from '../../assets/loading.gif';
 import './style.css';
 
 
@@ -14,13 +16,19 @@ export default class notes extends Component {
   state = {
     search: '',
     url:'',
-
+    title:'',
+    artist:'',
+    press:false
   }
   play = async () => {
-
+    this.setState({search:''})
+    this.setState({press:true})
     const response = await api.get(`/${this.state.search}`);
     this.setState({url:response.config.url})
     console.log(response.config.url)
+    this.setState({title:response.data.title, artist:response.data.artist})
+    console.log(response.data)
+    this.setState({press:false})
   }
 
   render() {
@@ -38,12 +46,21 @@ export default class notes extends Component {
           </button>
         </div>
         <div class="play">
-          { !this.state.url && 
-            <span class='message'>Aguarde alguns segundos</span>
+          {this.state.press &&
+
+            <img class='img' src={loading} alt="Logo" />
+
+          }
+
+          { !this.state.url && !this.state.press && 
+            <>
+            <span class='message'>Após digitar a musica aguarde 15 segundos :D ...</span>
+            </>
           }
           { !!this.state.url && 
          
-          <div class="play-music">          
+          <div class="play-music">
+            <h3 class='name'>{this.state.title} - {this.state.artist}</h3>          
             <audio controls="controls">
               <source src={this.state.url} type='audio/mpeg'/>
                Your browser does not support the audio element.
